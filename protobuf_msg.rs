d223dda67048b7806865d36bfa44a3294661167f
VnomArrow <39835019+VnomArrow@users.noreply.github.com>
2021-11-10 12:40:59 +0100

    Add files via upload

---
+++ protobuf_msg.rs
@@ -0,0 +1,53 @@
+use prost;
+use prost::{Message, Enumeration};
+
+#[derive(Clone, PartialEq, Message)]
+pub struct SomeMessage {
+    #[prost(int32, tag = "1")]
+    pub action: i32,
+
+    #[prost(string, tag = "2")]
+    pub filename: String,
+
+    #[prost(tag = "3", bytes)]
+    pub data: Vec<u8>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Enumeration)]
+    #[repr(i32)]
+    pub enum Action {
+        DeleteFile = 0,
+        AddFile = 1,
+        GetFile = 2,
+        GetFileList = 3,
+        Login = 4,
+        Logout = 5,
+        Error = 6,
+    }
+
+impl SomeMessage {
+    pub fn to_string(&self) -> String {
+        let mut out_str = String::new();
+        out_str += "action: '";
+        out_str += self.action.to_string().as_str();
+        out_str += "', filename: '";
+        out_str += self.filename.as_str();
+        out_str += "', data_len: '";
+        out_str += self.data.len().to_string().as_str();
+        out_str += "'";
+        return out_str;
+    }
+}
+
+pub fn example() {
+    let msg = SomeMessage{
+        action: Action::AddFile.into(),
+        filename: "somefile".into(),
+        data: vec!()
+    };
+    println!("before: {}", msg.to_string());
+    let mut buf: Vec<u8> = vec!();
+    let _ = msg.encode(&mut buf);
+    let read = SomeMessage::decode(&*buf).unwrap();
+    println!("after: {}", read.to_string());
+}
